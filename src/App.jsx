4b818acabeac48ab8ba4152e6fba9f9494a51b53
ng-jim <j.hayes@nittygritty.net>
2024-10-14 03:04:16 +0100

    Added MeasuringComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-import Comp1 from "./components/Comp1"
-import './App.css'
+import MeasuringComp from "./components/MeasuringComp";
+import Comp1 from "./components/Comp1";
+import "./App.css";
 
 function App() {
   return (
-    <Comp1></Comp1>
-  )
+    <>
+      <Comp1></Comp1>
+      <MeasuringComp></MeasuringComp>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MeasuringComp.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
+const MeasuringComp = () => {
+  const refContainer = useRef();
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    if (refContainer.current) {
+      setDimensions({
+        width: refContainer.current.offsetWidth,
+        height: refContainer.current.offsetHeight,
+      });
+    }
+  }, []);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+        backgroundColor: "#fafafa",
+        margin: "20px",
+      }}
+      ref={refContainer}
+    >
+      <pre>
+        Container:
+        <br />
+        width: {dimensions.width}
+        <br />
+        height: {dimensions.height}
+      </pre>
+    </div>
+  );
+};
+
+export default MeasuringComp;
